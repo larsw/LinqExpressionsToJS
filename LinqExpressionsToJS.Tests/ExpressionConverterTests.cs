9b40d2eaf1e6698c373d1d3cff5086f8b7526c65
Lars Wilhelmsen <lars@sral.org>
2012-11-09 10:15:30 +0100

    Initial commit

---
+++ LinqExpressionsToJS.Tests/ExpressionConverterTests.cs
@@ -0,0 +1,56 @@
+﻿namespace LinqExpressionsToJS.Tests
+{
+    using System;
+    using System.IO;
+    using System.Linq.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExpressionConverterTests
+    {
+        [TestMethod]
+        public void Should_convert_simple_expression_to_javascript_function()
+        {
+            var convert = new ExpressionConverter();
+            int a, b, c;
+            a = b = c = 5;
+            Expression<Func<int>> expression = () => a + b - c * a / b;
+            var result = expression.Compile()();
+            string javascript = convert.Convert(expression);
+            Assert.AreEqual("function() {a+b-c*a/b}", javascript);
+        }
+
+        [TestMethod]
+        public void Now_something_a_bit_more_advanced()
+        {
+            var convert = new ExpressionConverter();
+            var fieldService = new FieldService();
+            convert.MapToArray("fieldService.GetField", "felter");
+            Expression<Func<Field>> bodyExpr = () => fieldService.GetField(123);
+            string javascript = convert.Convert(bodyExpr);
+            File.WriteAllText(@"c:\temp\Now_something_a_bit_more_advanced.js", string.Format("var foo = {0};", javascript));
+            Assert.IsNotNull(javascript);
+        }
+    }
+
+    public class FieldService
+    {
+        public Field GetField(int fieldId)
+        {
+            return new Field();
+        }
+    }
+
+    public static class Fields
+    {
+        public static Field Id(int index)
+        {
+            return new Field();
+        }
+    }
+
+    public class Field
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ LinqExpressionsToJS/ExpressionConverter.cs
@@ -0,0 +1,122 @@
+﻿namespace LinqExpressionsToJS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Linq.Expressions;
+
+    public class ExpressionConverter
+    {
+        private readonly Visitor _visitor = new Visitor();
+
+        public string Convert(LambdaExpression expression)
+        {
+            return string.Concat("function() { return ", _visitor.Visit(expression), "}");
+        }
+
+        private class Visitor : DynamicExpressionVisitor
+        {
+            private readonly Dictionary<string, string> _arrayMaps = new Dictionary<string, string>();
+
+            private readonly StringBuilder _builder = new StringBuilder();
+            private readonly Stack<string> _stack = new Stack<string>();
+
+            public Dictionary<string, string> ArrayMaps
+            {
+                get { return _arrayMaps; }
+            }
+
+            public string Visit(LambdaExpression expression)
+            {
+                base.Visit(expression);
+                //while (_stack.Count > 0) _builder.Append(_stack.Pop());
+                return _builder.ToString();
+            }
+
+            protected override Expression VisitMember(MemberExpression node)
+            {
+                _stack.Push(node.Member.Name);
+                //_builder.Append(node.Member.Name);
+                return base.VisitMember(node);
+            }
+
+            protected override Expression VisitBinary(BinaryExpression node)
+            {
+                var left = Visit(node.Left);
+                
+                _stack.Push(BinaryNodeTypeToOperator(node.NodeType).ToString());
+                var right = Visit(node.Right);
+                return node;
+            }
+
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                var ret = base.VisitMethodCall(node);
+                var constant = _stack.Pop();
+                var fieldName = _stack.Pop();
+                var methodName = node.Method.Name;
+                var fieldAndMethod = string.Format("{0}.{1}", fieldName, methodName);
+                string mapTo;
+                if (_arrayMaps.TryGetValue(fieldAndMethod, out mapTo))
+                {
+                    _builder.AppendFormat("{0}[{1}]", mapTo, constant);                    
+                }
+                else
+                {
+                    _builder.AppendFormat("{0}({1})", fieldAndMethod, constant);                                        
+                }
+
+                return ret;
+            }
+
+            protected override Expression VisitConstant(ConstantExpression node)
+            {
+                if (false == IsCompilerGenerated(node.Type))
+                {
+                    _stack.Push(node.Value.ToString());
+                }
+
+                return base.VisitConstant(node);
+            }
+
+            private char BinaryNodeTypeToOperator(ExpressionType nodeType)
+            {
+                char op;
+                switch (nodeType)
+                {
+                    case ExpressionType.Add:
+                        op = '+';
+                        break;
+                    case ExpressionType.Subtract:
+                        op = '-';
+                        break;
+                    case ExpressionType.Multiply:
+                        op = '*';
+                        break;
+                    case ExpressionType.Divide:
+                        op = '/';
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+                return op;
+            }
+
+            private bool IsCompilerGenerated(Type t)
+            {
+                if (t == null)
+                    return false;
+
+                return t.IsDefined(typeof(CompilerGeneratedAttribute), false)
+                    || IsCompilerGenerated(t.DeclaringType);
+            }
+        }
+
+        public void MapToArray(string fieldAndMethodName, string arrayName)
+        {
+            _visitor.ArrayMaps.Add(fieldAndMethodName, arrayName);
+        }
+
+    }
+}
